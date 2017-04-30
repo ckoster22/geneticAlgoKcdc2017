6bb38f8205dcc2cd7f43fd0ab8be6683e95fd30a
Charlie Koster <ckoster22@gmail.com>
2017-04-29 22:07:14 -0500

    Triangle variation

--- src/evolvingImages/evolving.images.js
+++ src/evolvingImages/evolving.images.js
@@ -3,24 +3,30 @@
 import { evolveSolution } from '../genetic.algo';
 import type { GAOptions, Organism, MaybeOrganism } from '../genetic.algo';
 
-type Circle = {
-    x: number,
-    y: number,
-    radius: number,
+// type Point = {
+//     x: number,
+//     y: number
+// };
+type Triangle = {
+    point1x: number,
+    point1y: number,
+    point2x: number,
+    point2y: number,
+    point3x: number,
+    point3y: number,
     red: number,
     green: number,
     blue: number,
     alpha: number,
 };
-type Dna = Array<Circle>;
+type Dna = Array<Triangle>;
 
 const WIDTH: number = 640;
 const HEIGHT: number = 480;
 const CROSSOVER_RATE: number = 0.3;
 const MUTATION_RATE: number = 0.03;
 
-let NEW_CIRCLE_THRESHOLD: number = 0.99;
-let RADIUS: number = 200;
+let NEW_TRIANGLE_THRESHOLD: number = 0.99;
 
 let generation: number = 0;
 let images: any = [];
@@ -34,7 +40,7 @@ const generateRandomOrganism = (): Organism<Dna> => {
     const randDna: Dna = [];
 
     for (let i = 0; i < 5; ++i) {
-        randDna.push(getRandomCircle());
+        randDna.push(getRandomTriangle());
     }
 
     return {
@@ -42,11 +48,14 @@ const generateRandomOrganism = (): Organism<Dna> => {
         score: null
     };
 };
-const getRandomCircle = (): Circle => {
+const getRandomTriangle = (): Triangle => {
     return {
-        x: Math.floor(Math.random() * WIDTH),
-        y: Math.floor(Math.random() * HEIGHT),
-        radius: Math.floor(RADIUS),
+        point1x: Math.floor(Math.random() * WIDTH),
+        point1y: Math.floor(Math.random() * HEIGHT),
+        point2x: Math.floor(Math.random() * WIDTH),
+        point2y: Math.floor(Math.random() * HEIGHT),
+        point3x: Math.floor(Math.random() * WIDTH),
+        point3y: Math.floor(Math.random() * HEIGHT),
         red: Math.floor(Math.random() * 255),
         green: Math.floor(Math.random() * 255),
         blue: Math.floor(Math.random() * 255),
@@ -94,93 +103,128 @@ const drawOrganism = (organism: Organism<Dna>): void => {
     context.clearRect(0, 0, WIDTH, HEIGHT);
 
     for (let i = 0; i < organism.dna.length; ++i) {
-        let circle: Circle = organism.dna[i];
+        let triangle: Triangle = organism.dna[i];
         context.beginPath();
-        context.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI, false);
-        context.fillStyle = 'rgba(' + circle.red + ', ' + circle.green + ', ' + circle.blue + ', ' + circle.alpha + ')';
+        context.moveTo(triangle.point1x, triangle.point1y);
+        context.lineTo(triangle.point2x, triangle.point2y);
+        context.lineTo(triangle.point3x, triangle.point3y);
+        context.fillStyle = 'rgba(' + triangle.red + ', ' + triangle.green + ', ' + triangle.blue + ', ' + triangle.alpha + ')';
         context.fill();
     }
 }
 
 const crossoverDnas = (dna1: Dna, dna2: Dna): Dna => {
-    let childCircles = dna1.slice(0, Math.floor(CROSSOVER_RATE * dna1.length));
-    childCircles = childCircles.concat(dna2.slice(Math.floor(CROSSOVER_RATE * dna2.length)));
+    let dnaParts = dna1.slice(0, Math.floor(CROSSOVER_RATE * dna1.length));
+    dnaParts = dnaParts.concat(dna2.slice(Math.floor(CROSSOVER_RATE * dna2.length)));
 
-    return childCircles;
+    return dnaParts;
 };
 
 const mutateDna = (dna: Dna): Dna => {
-    if (Math.random() >= NEW_CIRCLE_THRESHOLD) {
+    if (Math.random() >= NEW_TRIANGLE_THRESHOLD) {
         const nextDna = dna.slice();
-        nextDna.push(getRandomCircle());
+        nextDna.push(getRandomTriangle());
 
         return nextDna;
     } else {
         const randIndex: number = Math.floor(Math.random() * dna.length);
-        const currentCircle: Circle = dna[randIndex];
+        const currentTriangle: Triangle = dna[randIndex];
 
-        const nextCircle = mutateCircle(currentCircle);
-        return [...dna.slice(0, randIndex), nextCircle, ...dna.slice(randIndex+1)];
+        const nextTriangle = mutateTriangle(currentTriangle);
+        return [...dna.slice(0, randIndex), nextTriangle, ...dna.slice(randIndex+1)];
     }
 }
 
-const mutateCircle = (currentCircle: Circle): Circle => {
-    const randAttr: number = Math.floor(Math.random() * 7);
-    const lowerX: number = Math.max(0, currentCircle.x - currentCircle.x * MUTATION_RATE);
-    const upperX: number = Math.min(WIDTH, currentCircle.x + currentCircle.x * MUTATION_RATE);
-    const lowerY: number = Math.max(0, currentCircle.y - currentCircle.y * MUTATION_RATE);
-    const upperY: number = Math.min(HEIGHT, currentCircle.y + currentCircle.y * MUTATION_RATE);
-    const lowerRadius: number = Math.max(0, currentCircle.radius - currentCircle.radius * MUTATION_RATE);
-    const upperRadius: number = Math.min(300, currentCircle.radius + currentCircle.radius * MUTATION_RATE);
-    const lowerRed: number = Math.max(0, currentCircle.red - currentCircle.red * MUTATION_RATE);
-    const upperRed: number = Math.min(255, currentCircle.red + currentCircle.red * MUTATION_RATE);
-    const lowerGreen: number = Math.max(0, currentCircle.green - currentCircle.green * MUTATION_RATE);
-    const upperGreen: number = Math.min(255, currentCircle.green + currentCircle.green * MUTATION_RATE);
-    const lowerBlue: number = Math.max(0, currentCircle.blue - currentCircle.blue * MUTATION_RATE);
-    const upperBlue: number = Math.min(255, currentCircle.blue + currentCircle.blue * MUTATION_RATE);
-    let nextCircle;
+const mutateTriangle = (currentTriangle: Triangle): Triangle => {
+    const randAttr: number = Math.floor(Math.random() * 9);
+    const lowerPoint1X: number = Math.max(0, currentTriangle.point1x - currentTriangle.point1x * MUTATION_RATE);
+    const upperPoint1X: number = Math.min(WIDTH, currentTriangle.point1x + currentTriangle.point1x * MUTATION_RATE);
+    const lowerPoint1Y: number = Math.max(0, currentTriangle.point1y - currentTriangle.point1y * MUTATION_RATE);
+    const upperPoint1Y: number = Math.min(HEIGHT, currentTriangle.point1y + currentTriangle.point1y * MUTATION_RATE);
+    const lowerPoint2X: number = Math.max(0, currentTriangle.point2x - currentTriangle.point2x * MUTATION_RATE);
+    const upperPoint2X: number = Math.min(WIDTH, currentTriangle.point2x + currentTriangle.point2x * MUTATION_RATE);
+    const lowerPoint2Y: number = Math.max(0, currentTriangle.point2y - currentTriangle.point2y * MUTATION_RATE);
+    const upperPoint2Y: number = Math.min(HEIGHT, currentTriangle.point2y + currentTriangle.point2y * MUTATION_RATE);
+    const lowerPoint3X: number = Math.max(0, currentTriangle.point3x - currentTriangle.point3x * MUTATION_RATE);
+    const upperPoint3X: number = Math.min(WIDTH, currentTriangle.point3x + currentTriangle.point3x * MUTATION_RATE);
+    const lowerPoint3Y: number = Math.max(0, currentTriangle.point3y - currentTriangle.point3y * MUTATION_RATE);
+    const upperPoint3Y: number = Math.min(HEIGHT, currentTriangle.point3y + currentTriangle.point3y * MUTATION_RATE);
+    const lowerRed: number = Math.max(0, currentTriangle.red - currentTriangle.red * MUTATION_RATE);
+    const upperRed: number = Math.min(255, currentTriangle.red + currentTriangle.red * MUTATION_RATE);
+    const lowerGreen: number = Math.max(0, currentTriangle.green - currentTriangle.green * MUTATION_RATE);
+    const upperGreen: number = Math.min(255, currentTriangle.green + currentTriangle.green * MUTATION_RATE);
+    const lowerBlue: number = Math.max(0, currentTriangle.blue - currentTriangle.blue * MUTATION_RATE);
+    const upperBlue: number = Math.min(255, currentTriangle.blue + currentTriangle.blue * MUTATION_RATE);
+    let nextTriangle;
 
     switch (randAttr) {
         case 0:
-            nextCircle = Object.assign({}, currentCircle, {
-                x: mutateValue(currentCircle.x, lowerX, upperX)
-            })
+            nextTriangle = {
+                ...currentTriangle,
+                point1x: mutateValue(currentTriangle.point1x, lowerPoint1X, upperPoint1X)
+            }
             break;
         case 1:
-            nextCircle = Object.assign({}, currentCircle, {
-                y: mutateValue(currentCircle.y, lowerY, upperY)
-            })
+            nextTriangle = {
+                ...currentTriangle,
+                point1y: mutateValue(currentTriangle.point1y, lowerPoint1Y, upperPoint1Y)
+            }
+            break;
+        case 1:
+            nextTriangle = {
+                ...currentTriangle,
+                point2x: mutateValue(currentTriangle.point2x, lowerPoint2X, upperPoint2X)
+            }
             break;
         case 2:
-            nextCircle = Object.assign({}, currentCircle, {
-                radius: mutateValue(currentCircle.radius, lowerRadius, upperRadius)
-            })
+            nextTriangle = {
+                ...currentTriangle,
+                point2y: mutateValue(currentTriangle.point2y, lowerPoint2Y, upperPoint2Y)
+            }
             break;
         case 3:
-            nextCircle = Object.assign({}, currentCircle, {
-                red: Math.round(mutateValue(currentCircle.red, lowerRed, upperRed))
-            })
+            nextTriangle = {
+                ...currentTriangle,
+                point3x: mutateValue(currentTriangle.point3x, lowerPoint3X, upperPoint3X)
+            }
             break;
         case 4:
-            nextCircle = Object.assign({}, currentCircle, {
-                green: Math.round(mutateValue(currentCircle.green, lowerGreen, upperGreen))
-            })
+            nextTriangle = {
+                ...currentTriangle,
+                point3y: mutateValue(currentTriangle.point3y, lowerPoint3Y, upperPoint3Y)
+            }
             break;
         case 5:
-            nextCircle = Object.assign({}, currentCircle, {
-                blue: Math.round(mutateValue(currentCircle.blue, lowerBlue, upperBlue))
-            })
+            nextTriangle = {
+                ...currentTriangle,
+                red: Math.round(mutateValue(currentTriangle.red, lowerRed, upperRed))
+            }
             break;
         case 6:
-            nextCircle = Object.assign({}, currentCircle, {
-                alpha: parseFloat(mutateValue(currentCircle.alpha, 0, 1).toFixed(2))
-            })
+            nextTriangle = {
+                ...currentTriangle,
+                green: Math.round(mutateValue(currentTriangle.green, lowerGreen, upperGreen))
+            }
+            break;
+        case 7:
+            nextTriangle = {
+                ...currentTriangle,
+                blue: Math.round(mutateValue(currentTriangle.blue, lowerBlue, upperBlue))
+            }
+            break;
+        case 8:
+            nextTriangle = {
+                ...currentTriangle,
+                alpha: parseFloat(mutateValue(currentTriangle.alpha, 0, 1).toFixed(2))
+            }
             break;
         default:
-            nextCircle = Object.assign({}, currentCircle);
+            nextTriangle = {
+                ...currentTriangle
+            }
     }
 
-    return nextCircle;
+    return nextTriangle;
 };
 
 const mutateValue = (current: number, min: number, max: number): number => {
@@ -212,17 +256,7 @@ const genCB = (maybeOrganism: MaybeOrganism<Dna>) => {
     console.log(generation);
 
     if (generation % 1000 === 0) {
-        NEW_CIRCLE_THRESHOLD *= 0.995;
-    }
-
-    if (generation % 120 === 0) {
-        const newRadius = RADIUS * 0.98;
-
-        if (Math.floor(newRadius) === Math.floor(RADIUS)) {
-            RADIUS = Math.max(1, RADIUS - 1);
-        } else {
-            RADIUS = newRadius
-        }
+        NEW_TRIANGLE_THRESHOLD *= 0.995;
     }
 
     if (generation % 5 === 0) {
